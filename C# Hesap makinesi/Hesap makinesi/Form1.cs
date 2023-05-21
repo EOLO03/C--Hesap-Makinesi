98fe1981aa0cdb7e47023055d15bf8a4f3f4ff7a
Sabri Alperen Kılıç <75065926+EOLO03@users.noreply.github.com>
2023-05-21 18:05:00 +0300

    Basit hesap makinesi.
    Önceden C# ile form app yazmayı öğrenmek için yazdığım basit hesap makinesi.

---
+++ C# Hesap makinesi/Hesap makinesi/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hesap_makinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayı1;
+            int sayı2;
+            int sonuç;
+
+                sayı1 = Convert.ToInt32(textBox1.Text); 
+                sayı2 = Convert.ToInt32(textBox2.Text);
+
+            sonuç = sayı1 + sayı2;
+            label4.Text = sonuç.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sayı1;
+            int sayı2;
+            int sonuç;
+
+            sayı1 = Convert.ToInt32(textBox1.Text);
+            sayı2 = Convert.ToInt32(textBox2.Text);
+
+            sonuç = sayı1 - sayı2;
+            label4.Text = sonuç.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int sayı1;
+            int sayı2;
+            int sonuç;
+
+            sayı1 = Convert.ToInt32(textBox1.Text);
+            sayı2 = Convert.ToInt32(textBox2.Text);
+
+            sonuç = sayı1 * sayı2;
+            label4.Text = sonuç.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int sayı1;
+            int sayı2;
+            int sonuç;
+
+            sayı1 = Convert.ToInt32(textBox1.Text);
+            sayı2 = Convert.ToInt32(textBox2.Text);
+
+            sonuç = sayı1 / sayı2;
+            label4.Text = sonuç.ToString();
+        }
+    } // TÜM BU ATANILMIŞ DEĞİŞKENLERDE BUTON 1 İÇİN TOPLAMA, BUTON 2 İÇİN ÇIKARMA, BUTON 3 İÇİN ÇARPMA VE BUTON 4 İÇİN BÖLME İŞLEMİ YAPLILMASI İSTENİLMİŞTİR.
+}
